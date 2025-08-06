5c367056b14e0273a6a3327e62444c8d765b56ee
Juan Carlos <Alcalajcm0827@gmail.com>
2025-08-06 18:57:58 -0500

    cambio

--- inventario_cysla/inventario_cysla/settings.py
+++ inventario_cysla/inventario_cysla/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'inventario_cysla.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django'
+        'ENGINE': 'django.db.backends.mysql'
 ,
          'NAME': os.environ.get('DB_NAME', 'bm0b5qenkdqmsryywkfw'),
         'USER': os.environ.get('DB_USER', 'uphc863xus8ue0e1'),
